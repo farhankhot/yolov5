c6f47c7e7a45a1768dfac97240d0b83b1bc0a125
Farhan K <farhan.crazyeyes@gmail.com>
2022-09-01 07:22:58 -0700

    Update common.py
    Signed-off-by: Farhan K <farhan.crazyeyes@gmail.com>

--- models/common.py
+++ models/common.py
@@ -698,9 +698,6 @@ class Detections:
                         if box_coord is not None:
                             annotator = Annotator(im, example=str(self.names))
                             annotator.box_label(box, "blah", color=colors(cls))
-                            im = Image.fromarray(im.astype(np.uint8)) if isinstance(im, np.ndarray) else im
-                            f = self.files[i]
-                            im.save("my stuff" / f)
 
                     im = annotator.im
                 
@@ -724,8 +721,8 @@ class Detections:
                 LOGGER.info(f'Saved results to {save_dir}\n')
             return crops
 
-    def my_box_and_label(self):
-        self.display(box_coord = 1)      
+    def my_box_and_label(self, save_dir='runs/detect/my'):
+        self.display(save=True, box_coord = 1, save_dir=save_dir)      
     
     def print(self):
         self.display(pprint=True)  # print results
